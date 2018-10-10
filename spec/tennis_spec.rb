8dbcbda0ea74788036daa3556d131a2e8e376e6e
Elena <elena@madetech.com>
2018-10-10 16:57:58 +0100

    making the first test pass

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,5 +1,16 @@
+class TennisScorer
+
+  def current_score
+    "0-0"
+  end
+end
+
 describe TennisScorer do
   it 'should score a new game where neither player has scored yet' do
+
+    tennis_scorer  = TennisScorer.new
+    score = tennis_scorer.current_score
+
     expect(score).to eq("0-0")
   end
 end
