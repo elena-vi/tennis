be7be7babdaf94fff0fc5344e4bbbe5a8b2b1b0c
Elena <elena@madetech.com>
2018-10-12 10:41:46 +0100

    4 tests, prod codes needs to be refactored

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,5 +1,4 @@
 class TennisScorer
-
   def initialize
     @score = "0-0"
   end
@@ -7,6 +6,8 @@ class TennisScorer
   def point_won
     if @score == "0-0"
       @score  = "15-0"
+    elsif @score == "30-0"
+      @score = "40-0"
     else
       @score = "30-0"
     end
@@ -17,27 +18,38 @@ class TennisScorer
   end
 end
 
+
+
 describe TennisScorer do
+
+  def expect_score_to_eq(expected_score)
+    expect(tennis_scorer.current_score).to eq(expected_score)
+  end
+
+  let(:tennis_scorer) do
+    tennis_scorer = TennisScorer.new
+  end
+
   it 'should score a new game where neither player has scored yet' do
-
-    tennis_scorer  = TennisScorer.new
-    score = tennis_scorer.current_score
-
-    expect(score).to eq("0-0")
+    expect_score_to_eq("0-0")
   end
 
   it 'should score a game where player one has scored once' do
-    tennis_scorer  = TennisScorer.new
     tennis_scorer.point_won
-    score = tennis_scorer.current_score
-    expect(score).to eq("15-0")
+    expect_score_to_eq("15-0")
   end
 
   it 'should score a game where player one has scored twice' do
-    tennis_scorer  = TennisScorer.new
-    tennis_scorer.point_won
-    tennis_scorer.point_won
-    score = tennis_scorer.current_score
-    expect(score).to eq("30-0")
+    2.times do
+      tennis_scorer.point_won
+    end
+    expect_score_to_eq("30-0")
+  end
+
+  it 'should score a game where player one has scored thrice' do
+    3.times do
+      tennis_scorer.point_won
+    end
+    expect_score_to_eq("40-0")
   end
 end
