032b2f48a98a921f5fb984ad3aacbb3e4130ade5
Elena <elena@madetech.com>
2018-10-12 15:08:42 +0100

    refactoring if statements

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -20,24 +20,37 @@ class TennisScorer
     if SCORE[@score[player]]
       @score[player] = SCORE[@score[player]]
       @game_state = "DEUCE" if deuce?
-    elsif deuce? && @game_state == "PLAYER 1 ADVANTAGE"
-      @game_state = "PLAYER 1 WINS"
-    elsif deuce? && @game_state == "PLAYER 2 ADVANTAGE"
-      @game_state = "PLAYER 2 WINS"
     else
-      @game_state = "PLAYER 1 WINS" if (@score[:player1] > @score[:player2])
-      @game_state = "PLAYER 2 WINS" if @score[:player1] < @score[:player2]
-      @game_state = "PLAYER 1 ADVANTAGE" if deuce? && player == :player1
-      @game_state = "PLAYER 2 ADVANTAGE" if deuce? && player == :player2
+      return @game_state = "PLAYER 1 WINS" if player_1_win?(player)
+      return @game_state = "PLAYER 2 WINS" if player_2_win?(player)
+      return @game_state = "PLAYER 1 ADVANTAGE" if player_1_advantage?(player)
+      return @game_state = "PLAYER 2 ADVANTAGE" if player_2_advantage?(player)
     end
   end
 
   def current_score
     return @game_state if @game_state
+
     "#{@score[:player1]}-#{@score[:player2]}"
   end
 
   private
+
+  def player_2_advantage?(player)
+    deuce? && player == :player2
+  end
+
+  def player_1_advantage?(player)
+    deuce? && player == :player1
+  end
+
+  def player_2_win?(player)
+    @score[:player1] < @score[:player2] || deuce? && @game_state == "PLAYER 2 ADVANTAGE" && player == :player2
+  end
+
+  def player_1_win?(player)
+    @score[:player1] > @score[:player2] || deuce? && @game_state == "PLAYER 1 ADVANTAGE" && player == :player1
+  end
 
   def deuce?
     @score[:player1] == 40 && @score[:player2] == 40
@@ -140,5 +153,13 @@ describe TennisScorer do
     end
     2.times{tennis_scorer.point_won(:player2)}
     expect_score_to_eq("PLAYER 2 WINS")
+  end
+
+  xit 'should score a game where player two have scored once after player one advantage' do
+    4.times do
+      tennis_scorer.point_won(:player1)
+      tennis_scorer.point_won(:player2)
+    end
+    expect_score_to_eq("DEUCE")
   end
 end
