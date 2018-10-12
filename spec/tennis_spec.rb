4e82e494dbe639407380d52dfc1ab1f8b3b30e14
Elena <elena@madetech.com>
2018-10-12 11:11:53 +0100

    handling advantages

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -19,6 +19,8 @@ class TennisScorer
 
   def current_score
     return "DEUCE" if @score[:player1] == 40 && @score[:player2] == 40
+    return "PLAYER 1 ADVANTAGE" unless @score[:player1]
+    return "PLAYER 2 ADVANTAGE" unless @score[:player2]
 
     "#{@score[:player1]}-#{@score[:player2]}"
   end
@@ -28,10 +30,6 @@ describe TennisScorer do
 
   def expect_score_to_eq(expected_score)
     expect(tennis_scorer.current_score).to eq(expected_score)
-  end
-
-  let(:tennis_scorer) do
-    tennis_scorer = TennisScorer.new
   end
 
   it 'should score a new game where neither player has scored yet' do
@@ -74,5 +72,23 @@ describe TennisScorer do
       tennis_scorer.point_won(:player2)
     end
     expect_score_to_eq("DEUCE")
+  end
+
+  it 'should score a game where player one have scored once after deuce' do
+    3.times do
+      tennis_scorer.point_won(:player1)
+      tennis_scorer.point_won(:player2)
+    end
+    tennis_scorer.point_won(:player1)
+    expect_score_to_eq("PLAYER 1 ADVANTAGE")
+  end
+
+  it 'should score a game where player two have scored once after deuce' do
+    3.times do
+      tennis_scorer.point_won(:player1)
+      tennis_scorer.point_won(:player2)
+    end
+    tennis_scorer.point_won(:player2)
+    expect_score_to_eq("PLAYER 2 ADVANTAGE")
   end
 end
